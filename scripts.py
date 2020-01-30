2153b71b3ea9ab45016e9aea8648cd3073d5fa61
Alexander Neupokoev <admin@armavir.ru>
2020-01-30 10:16:18 +0300

    refactoring code

--- scripts.py
+++ scripts.py
@@ -6,30 +6,30 @@ from datacenter.models import *
 
 def get_child(name):
     try:
-        child = Schoolkid.objects.get(full_name__contains=name)
+        child_info = Schoolkid.objects.get(full_name__contains=name)
     except Schoolkid.DoesNotExist:
         print('Введено не верное имя. Введите фамилию и имя полностью')
     except Schoolkid.MultipleObjectsReturned:
         print('Найдено несколько учеников по вашему запросу. Уточните запрос')
-    return child
+    return child_info
 
 
 def fix_marks(schoolkid):
-    child = get_child(schoolkid)
-    marks = Mark.objects.filter(schoolkid=child, points__lt=4)
+    child_info = get_child(schoolkid)
+    marks = Mark.objects.filter(schoolkid=child_info, points__lt=4)
     for mark in marks:
         mark.points = 5
         mark.save()
 
 
 def remove_chastisements(schoolkid):
-    child = get_child(schoolkid)
-    chastiments = Chastisement.objects.filter(schoolkid=child)
+    child_info = get_child(schoolkid)
+    chastiments = Chastisement.objects.filter(schoolkid=child_info)
     chastiments.delete()
 
 
 def get_last_lesson_subject_for_child(child, subject):
-    child = get_for_child(schoolkid)
+    child_info = get_for_child(schoolkid)
     lessons = Lesson.objects.filter(
         year_of_study=child.year_of_study, group_letter=child.group_letter)
     last_lesson = lessons.filter(
